731eaca1887b3ca8e89a2753dbc198bbfcc91379
SXC8976 <SXC8976@SSCVMDEVU35024.amer.homedepot.com>
2016-08-30 19:03:49 +0530

    add

--- src/com/homedepot/mm/pc/pacman/su/excel/SonarMain.java
+++ src/com/homedepot/mm/pc/pacman/su/excel/SonarMain.java
@@ -5,6 +5,7 @@ public class SonarMain {
 	public static void main(String[] args) {
 		
 		System.out.println("Sonar");
+		System.out.println("SonarQubeServer");
 		// TODO Auto-generated method stub
 
 	}
